e8bca370c79a25240256b644b94679ab681b80dc
Samuel Leßmann <samuel@lessmann.dev>
2025-08-21 13:57:40 +0200

    chore: bump toolchain to v4.23.0-rc2

--- lakefile.lean
+++ lakefile.lean
@@ -7,5 +7,4 @@ package InternalLanguage
 lean_lib InternalLanguage
 
 require aesop from git "https://github.com/JLimperg/aesop" @ "master"
-require std from git "https://github.com/leanprover/std4" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
